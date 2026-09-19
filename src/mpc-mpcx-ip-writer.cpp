b14ca746923735957ba44027172c681346703f02
nobukoba <kobayash@rcnp.osaka-u.ac.jp>
2026-09-20 00:15:18 +0900

    Fix shared RBCP default constant qualification

--- src/mpc-mpcx-ip-writer.cpp
+++ src/mpc-mpcx-ip-writer.cpp
@@ -12,8 +12,8 @@
 #include <vector>
 
 namespace {
-constexpr double DEFAULT_TIMEOUT = rbcp::DEFAULT_TIMEOUT;
-constexpr uint16_t DEFAULT_PORT = rbcp::DEFAULT_PORT;
+constexpr double DEFAULT_TIMEOUT = sitcp_sitcpxg::rbcp::DEFAULT_TIMEOUT;
+constexpr uint16_t DEFAULT_PORT = sitcp_sitcpxg::rbcp::DEFAULT_PORT;
 constexpr uint32_t EEPROM_BASE = 0xFFFFFC00u;
 constexpr uint32_t EEPROM_WRITE_ENABLE = 0xFFFFFCFFu;
 constexpr uint32_t XG_IDENTIFIER = 0xFFFFFF08u;
